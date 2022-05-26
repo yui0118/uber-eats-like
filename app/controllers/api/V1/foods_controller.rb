e55827b0a8613bbf45b2a36f13f16d2d3d32c497
yui0118 <yui.orange.hello@gmail.com>
2022-05-26 10:30:00 +0900

    Fix: restaurantとfoodコントローラーの修正

--- app/controllers/api/V1/foods_controller.rb
+++ app/controllers/api/V1/foods_controller.rb
@@ -3,10 +3,10 @@ module Api
     class FoodsController < ApplicationController
       def index
         # パラメーターで受け取った店舗IDに対応するレストランの情報を１つ取得する
-        # restaurant_idのレストランの食べ物を全て取得する
+        # restaurant_idのレストランの情報の中から、そのレストランに紐づく食べ物を全て取得する
         # JSON形式で指定のレストランにある食べ物一覧データを返却
         restaurant = Restaurant.find(params[:restaurant_id])
-        foods = restaurants.foods
+        foods = restaurant.foods
         render json: {
           foods: foods
         }, status: :ok
--- app/controllers/api/V1/restaurants_controller.rb
+++ app/controllers/api/V1/restaurants_controller.rb
@@ -4,7 +4,6 @@ module Api # moduleで名前空間を指定
       def index
         # Restaurantモデルのデータを全て取得して、restaurantsという変数に代入
         restaurants = Restaurant.all
-
         render json: { # JSON形式でデータを返却
           restaurants: restaurants
         }, status: :ok # リクエストが成功した場合に返すレスポンスコード200 OKをデータと一緒に返す
