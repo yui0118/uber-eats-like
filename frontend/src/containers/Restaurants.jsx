012c92fda1387266f808e90ffd5d8f0c7b3e067b
yui0118 <yui.orange.hello@gmail.com>
2022-07-24 16:17:17 +0900

    Add: useEffectを使ってRestaurantsコンポーネントの初期レンダリング時にのみAPI関数を一度だけ呼ぶ処理を追加する

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,13 @@
 import React, { Fragment } from "react";
+import { useEffect } from "react";
+// api関数をimport
+import { fetchRestaurants } from "../apis/restaurants";
 
 export const Restaurants = () => {
+  // useEffectを使ってコンポーネントの初期レンダリング時にのみAPI関数を一度だけ呼ぶ
+  // API関数の実行が成功した場合は、関数がres.dataを返すので、その結果を(data)というかたちで受け取り、APIの返り値をコンソールに出力する
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
   return <Fragment>レストラン一覧</Fragment>;
 };
